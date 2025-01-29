5a503a1a79cdd7e403245714c9ee667737cd86bc
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-01-29 16:43:06 +0200

    wip DeployComputeJob

---
+++ ignite-java-demo/src/main/java/org/example/DeployComputeJob.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class DeployComputeJob {
+    public static void main(String[] args) {
+        // https://www.gridgain.com/docs/gridgain9/latest/developers-guide/code-deployment/code-deployment
+        // /management/v1/deployment/units/{unitId}/{unitVersion}
+        // https://www.gridgain.com/sdk/gridgain9/latest/openapi.html#tag/deployment/operation/deployUnit
+    }
+}
