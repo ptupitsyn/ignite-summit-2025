32e075c3e193f03586d0fb32a6f546e49754ca5f
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-02-26 17:49:50 +0200

    Finalize demos (#1)
    * wip
    
    * Add .NET demo
    
    * Add RO TX
    
    * wip DeployComputeJob
    
    * wip
    
    * Remove staging repo
    
    * Add Docker Compose
    
    * wip
    
    * wip cluster init
    
    * wip cluster init
    
    * Partition awareness
    
    * Partition awareness
    
    * Partition awareness
    
    * Partition awareness
    
    * wip SQL demo
    
    * wip cleanup
    
    * wip
    
    * sendServerExceptionStackTraceToClient

---
+++ ignite-dotnet-demo/Program.cs
@@ -0,0 +1,68 @@
+﻿using Apache.Ignite;
+using Apache.Ignite.Sql;
+using Apache.Ignite.Table;
+using Apache.Ignite.Transactions;
+
+var cfg = new IgniteClientConfiguration("localhost");
+using IIgniteClient client = await IgniteClient.StartAsync(cfg);
+
+Console.WriteLine("Connected: " + client);
+
+// SQL API.
+await foreach (IIgniteTuple row in await client.Sql.ExecuteAsync(null, "SELECT * FROM Person"))
+{
+    Console.WriteLine(row);
+}
+
+// .NET APIs.
+ITable? table = await client.Tables.GetTableAsync("Person");
+
+// All table view types provide identical capabilities.
+IRecordView<IIgniteTuple> recordView = table!.RecordBinaryView;
+await recordView.UpsertAsync(null, new IgniteTuple { ["id"] = 2, ["name"] = "Jane" });
+
+IRecordView<Person> pocoView = table.GetRecordView<Person>();
+await pocoView.UpsertAsync(null, new Person(3, "Jack"));
+
+IKeyValueView<IIgniteTuple, IIgniteTuple> kvView = table.KeyValueBinaryView;
+await kvView.PutAsync(null, new IgniteTuple { ["id"] = 4 }, new IgniteTuple { ["name"] = "Jill" });
+
+IKeyValueView<int, string> kvPocoView = table.GetKeyValueView<int, string>();
+await kvPocoView.PutAsync(null, 5, "Joe");
+
+// SQL with mapping
+IResultSet<Person> query = await client.Sql.ExecuteAsync<Person>(null, "select * from Person");
+await foreach (Person person in query)
+{
+    Console.WriteLine(person);
+}
+
+// LINQ
+IQueryable<Person> queryable = pocoView.AsQueryable();
+
+var query2 = queryable
+    .Where(p => p.Id > 2)
+    .OrderBy(x => x.Id)
+    .Select(p => p.Name);
+
+Console.WriteLine("Generated SQL: " + query2);
+
+foreach (string name in query2)
+{
+    Console.WriteLine(name);
+}
+
+// Read-only TX: snapshot of data at the time of TX start.
+await using ITransaction roTx = await client.Transactions.BeginAsync(new TransactionOptions(ReadOnly: true));
+
+Option<Person> roTxRes1 = await pocoView.GetAsync(roTx, new Person(3, null!));
+
+await pocoView.UpsertAsync(transaction:null, new Person(3, "JACK-2")); // Update after RO TX.
+
+Option<Person> roTxRes2 = await pocoView.GetAsync(roTx, new Person(3, null!));
+Option<Person> actualRes = await pocoView.GetAsync(null, new Person(3, null!));
+
+Console.WriteLine($"RO TX result 1: {roTxRes1.Value}, 2: {roTxRes2.Value}, no tx result: {actualRes.Value}");
+
+// Model
+public record Person(int Id, string Name);
--- ignite-java-demo/src/main/java/org/example/Main.java
+++ ignite-java-demo/src/main/java/org/example/Main.java
@@ -12,15 +12,12 @@ import org.apache.ignite.table.Tuple;
 public class Main {
     public static void main(String[] args) {
         IgniteClient client = IgniteClient.builder()
-                .addresses("localhost")
+                .addresses("localhost:10800", "localhost:10801", "localhost:10802")
                 .build();
 
-        System.out.println("Connected to the cluster: " + client.connections().get(0));
+        System.out.println("Connected to the cluster: " + client.connections());
 
-        // SQL API.
-        client.sql().execute(null, "DROP TABLE IF EXISTS Person");
-        client.sql().execute(null, "CREATE TABLE Person (id INT PRIMARY KEY, name VARCHAR)");
-        client.sql().execute(null, "UPSERT INTO Person VALUES (1, 'John')");
+        // SQL API (use table created in CLI).
         client.sql().execute(null, "SELECT * FROM Person")
                 .forEachRemaining(x -> System.out.println(x.stringValue("name")));
 
@@ -49,14 +46,10 @@ public class Main {
         // Mix and match APIs.
         client.sql().execute(null, "SELECT * FROM Person2")
                 .forEachRemaining(x -> System.out.println(x.stringValue("name")));
-
-        // Compute
-        // TBD: Colocated update
     }
 
     public static class Person {
-        public Person() {
-        }
+        public Person() { }
 
         public Person(Integer id, String name) {
             this.id = id;
